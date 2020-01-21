20780e68ccbb5a2867a43f295ec5361dd72dac06
niki-01 <niki.manoledaki@gmail.com>
2020-01-21 13:51:42 +0000

    Functions for all three cases

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,23 @@
 var Javabuzz = function(){};
 
+Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
+};
+
 Javabuzz.prototype.isDivisibleByThree = function(number) {
-  return true;
+  return this._isDivisibleBy(number, 3);
+};
+
+Javabuzz.prototype.isDivisibleByFive = function(number) {
+  return this._isDivisibleBy(number, 5);
+};
+
+Javabuzz.prototype.isDivisibleByFifteen = function(number) {
+  return this._isDivisibleBy(number, 15);
+};
+
+Javabuzz.prototype.says = function(number) {
+  if (this.isDivisibleByThree(number)) {
+    return "Java";
+  }
 };
