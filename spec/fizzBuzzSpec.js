dd1802ca4999e83484ee65c26a8211f707cdf42a
niki-01 <niki.manoledaki@gmail.com>
2020-01-21 11:17:10 +0000

    First commit

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,11 @@
+describe("Javabuzz", function() {
+
+  var javabuzz;
+
+  describe('knows when a number is', function() {
+    it('divisible by 3', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+    });
+  }); 
+});
